5a4cc31fe6a81fbdb55a994b1c3f7fae523170fa
Jason Ryan Womack <jason.womack@solidfire.com>
2015-09-16 14:20:39 -0600

    SDK-83 - fix validator to allow Object types

--- jsvcgen/src/main/scala/com/solidfire/jsvcgen/codegen/Validator.scala
+++ jsvcgen/src/main/scala/com/solidfire/jsvcgen/codegen/Validator.scala
@@ -29,7 +29,8 @@ object Validator {
                           "number",
                           "null",
                           "float",
-                          "string"
+                          "string",
+                          "Object"
                         )
 }
 
