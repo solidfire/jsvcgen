dcdc2063e364648f3961bd02f08b45d2fa15ff72
Adam Haid <adam.haid@solidfire.com>
2015-11-16 11:18:37 -0700

    draft version of golang generator

--- project/common.scala
+++ project/common.scala
@@ -106,7 +106,7 @@ object Config {
 
 object Version {
   //this project
-  val jsvcgen = "0.1.16-SNAPSHOT"
+  val jsvcgen = "0.1.17-SNAPSHOT"
 
   val gson       = "2.3.1"
   val json4s     = "3.2.11"
