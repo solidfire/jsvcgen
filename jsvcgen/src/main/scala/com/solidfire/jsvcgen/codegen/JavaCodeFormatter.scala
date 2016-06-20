35549f05991e149994337a7e72ffba538cf69846
Jason Ryan Womack <jason.womack@solidfire.com>
2016-06-20 13:17:50 -0600

    SDK-301 - testing setup

--- jsvcgen/src/main/scala/com/solidfire/jsvcgen/codegen/JavaCodeFormatter.scala
+++ jsvcgen/src/main/scala/com/solidfire/jsvcgen/codegen/JavaCodeFormatter.scala
@@ -33,8 +33,8 @@ class JavaCodeFormatter( options: CliConfig, serviceDefintion: ServiceDefinition
       "boolean" -> "Boolean",
       "integer" -> "Long",
       "number" -> "Double",
-      "string" -> "String",
       "float" -> "Double",
+      "string" -> "String",
       "object" -> "java.util.Map<String, Object>"
     )
   )
@@ -42,23 +42,22 @@ class JavaCodeFormatter( options: CliConfig, serviceDefintion: ServiceDefinition
   private val primitives = Map(
     "boolean" -> (("boolean", "false")),
     "integer" -> (("long", "0")),
-    "number" -> (("double", "0.0")),
-    "float" -> (("double", "0.0")),
     "long" -> (("long", "0")),
-    "int" -> (("long", "0"))
+    "number" -> (("double", "0.0")),
+    "float" -> (("double", "0.0"))
   )
 
   def getTypeName( src: String, canBePrimitive: Boolean = false ): String = {
     if (canBePrimitive && primitives.contains( src.toLowerCase )) {
-      primitives.get(src).get._1
+      primitives.get( src ).get._1
     } else {
       directTypeNames.get( src )
-        .orElse( typeAliases.get( src ).map((alias: TypeUse) => getTypeName(alias.typeName, canBePrimitive) ) )
+        .orElse( typeAliases.get( src ).map( ( alias: TypeUse ) => getTypeName( alias.typeName, canBePrimitive && !alias.isOptional ) ) )
         .getOrElse( Util.camelCase( src, firstUpper = true ) )
     }
   }
 
-  def getTypeName( src: TypeDefinition ): String = getTypeName( src.name )
+  def getTypeName( src: TypeDefinition ): String = getTypeName( src.name, src.alias.isDefined )
 
   def getTypeName( src: TypeUse ): String = src match {
     case TypeUse( name, false, false, None ) => getTypeName( name, canBePrimitive = true )
@@ -133,7 +132,6 @@ class JavaCodeFormatter( options: CliConfig, serviceDefintion: ServiceDefinition
 
     val initializers = fields.map( {
       case (k, v) => v -> (
-
                           if (revSpecificMembers.contains( k )) {
                             if (k.typeUse.isOptional && k.typeUse.isArray)
                               s"""($v == null) ? Optional.<${getTypeName( k.typeUse.typeName )}[]>empty() : $v;"""
@@ -145,12 +143,12 @@ class JavaCodeFormatter( options: CliConfig, serviceDefintion: ServiceDefinition
                             s"""Optional.<${getTypeName( k.typeUse.typeName )}[]>empty();"""
                           } else if (k.typeUse.isOptional && !k.typeUse.isArray) {
                             s"""Optional.<${getTypeName( k.typeUse.typeName )}>empty();"""
-                          } else if(primitives.contains(k.typeUse.typeName)) {
-                            s"${primitives.get(k.typeUse.typeName).get._2};"
+                          } else if (primitives.contains( k.typeUse.typeName )) {
+                            s"${primitives.get( k.typeUse.typeName ).get._2};"
                           } else {
                             "null;"
                           })
-    } ).toList
+    }).toList
 
     val initializedFields = initializers.map( { case (v, f) => s"""        this.$v = $f""" } )
 
@@ -351,9 +349,15 @@ class JavaCodeFormatter( options: CliConfig, serviceDefintion: ServiceDefinition
       if (member.typeUse.isOptional) {
         val optionalArrayBrackets = if (member.typeUse.isArray) "[]" else ""
         sb ++=
-          s"""        public ${typeDefinition.name}.Builder optional${Util.camelCase( member.name, firstUpper = true )}(final ${ getTypeName( member.typeUse.typeName ) }$optionalArrayBrackets ${getFieldName( member )}) {\n"""
+          s"""        public ${typeDefinition.name}.Builder optional${Util.camelCase( member.name, firstUpper = true )}(final ${
+            getTypeName( member.typeUse
+              .typeName )
+          }$optionalArrayBrackets ${getFieldName( member )}) {\n"""
         sb ++=
-          s"""            this.${getFieldName( member )} = (${getFieldName( member )} == null) ? Optional.<${ getTypeName( member.typeUse.typeName ) }$optionalArrayBrackets>empty() : Optional.of(${getFieldName( member )});\n"""
+          s"""            this.${getFieldName( member )} = (${getFieldName( member )} == null) ? Optional.<${
+            getTypeName( member.typeUse
+              .typeName )
+          }$optionalArrayBrackets>empty() : Optional.of(${getFieldName( member )});\n"""
         sb ++= s"""            return this;\n"""
         sb ++= s"""        }\n\n"""
       } else {
