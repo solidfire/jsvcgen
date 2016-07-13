a015def94ceb8863b153443bedacb221ceae57b8
Jason Ryan Womack <jason.womack@solidfire.com>
2016-07-13 11:21:35 -0600

    SDK-313 - add new reflectioin package to osgi java client

--- jsvcgen/src/main/scala/com/solidfire/jsvcgen/codegen/JavaCodeFormatter.scala
+++ jsvcgen/src/main/scala/com/solidfire/jsvcgen/codegen/JavaCodeFormatter.scala
@@ -302,15 +302,22 @@ class JavaCodeFormatter( options: CliConfig, serviceDefintion: ServiceDefinition
       sb ++= s""";\n"""
     } else {
       sb ++= s""" {\n"""
-      if (useRequestObject) {
-        sb ++= s"""        return super.sendRequest("${method.name}", """
-        sb ++= s"""request, """
-        sb ++= s"""${getTypeName( method.name )}Request.class, """
-        sb ++= s"""${getTypeName( method.returnInfo ).split("<")(0)}.class """
-        sb ++= s""");\n"""
-      } else {
-        sb ++= s"""        return this.${getMethodName( method )}( new ${getTypeName( method.name )}Request(${getParameterUseList( method.params )}));\n"""
-      }
+      val hasValueAdaptor = method.returnInfo.get.adaptor.isDefined && method.returnInfo.get.adaptor.get.supports.contains("java")
+
+      val sendRequest =
+        if (useRequestObject) {
+          s"""super.sendRequest( "${method.name}", request, ${getTypeName( method.name )}Request.class, ${getTypeName( method.returnInfo ).split("<")(0)}.class )"""
+        } else {
+          s"""this.${getMethodName( method )}( new ${getTypeName( method.name )}Request( ${getParameterUseList( method.params )}))""".stripMargin
+        }
+
+      if(hasValueAdaptor) {
+          sb ++= s"""        final ${getTypeName( method.returnInfo )} result = $sendRequest;\n"""
+          sb ++= s"""\n"""
+          sb ++= s"""        return ${options.adaptorBase}.${method.returnInfo.get.adaptor.get.name}(request, result);\n"""
+        } else {
+          sb ++= s"""        return $sendRequest;\n"""
+        }
       sb ++= s"""    }\n"""
     }
 
