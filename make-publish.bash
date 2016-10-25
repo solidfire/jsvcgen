2c48345d79af9f940d57d5edaeeb6a80298028f7
Adam Haid <adam.haid@solidfire.com>
2016-10-25 11:00:32 -0600

    remove makes

--- make-publish.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-sbt '; + clean; + test; + publishSigned'
-echo copy assembly to sdk-java
-cp jsvcgen-assembly/target/jsvcgen-assembly-*-SNAPSHOT.jar ../solidfire-sdk-java
-echo copy assembly to sdk-python
-cp jsvcgen-assembly/target/jsvcgen-assembly-*-SNAPSHOT.jar ../solidfire-sdk-python
-echo copy assembly to sdk-dotnet
-cp jsvcgen-assembly/target/jsvcgen-assembly-*-SNAPSHOT.jar ../solidfire-sdk-dotnet
